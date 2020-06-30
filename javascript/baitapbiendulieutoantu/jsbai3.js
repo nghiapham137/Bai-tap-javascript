0a3717c41233e9ac8321f83ad023f9d684d4567b
nghiapham137 <66628234+nghiapham137@users.noreply.github.com>
2020-06-30 10:14:38 +0700

    Bo sung bai 3

---
+++ javascript/baitapbiendulieutoantu/jsbai3.js
@@ -0,0 +1,6 @@
+let inputBankinh;
+inputBankinh = prompt("Hãy nhập bán kính hình tròn:");
+let Bankinh = Math.pow(inputBankinh,2);
+let pi = Math.PI;
+let Dientich = Bankinh*pi;
+document.write("Diện tích hình tròn là: "+ Dientich);
